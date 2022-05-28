6b8e020e65dbfc1ba4dcfb03bc0ae4dd0ad95e19
Cody Peters <cjosephp15@gmail.com>
2022-05-29 01:02:10 +1000

    Read Covid data into an object

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express()
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.listen(5000, () => {
+    console.log('Server started on port 5000');
+})
--- public/js/choropleth.js
+++ public/js/choropleth.js
@@ -12,55 +12,79 @@ const projection = d3
 
 // Data and color scale
 let data = new Map();
-const colorScale = d3
+
+const rColorScale = d3
+  .scaleThreshold()
+  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+  .range(d3.schemeReds[7]);
+
+const gColorScale = d3
+  .scaleThreshold()
+  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+  .range(d3.schemeGreens[7]);
+
+const bColorScale = d3
   .scaleThreshold()
   .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
   .range(d3.schemeBlues[7]);
 
 // Tooltip
-const tooltip = d3.select("body")
+const tooltip = d3
+  .select("body")
   .append("div")
   .attr("class", "tooltip")
   .style("opacity", 0);
 
 // Load external data and boot
+const COVID_DATASET = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
+const COUNTRY_DATASET = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+
+var covid_data = {}
+
 Promise.all([
   d3.json(
-    "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+    COUNTRY_DATASET
   ),
   d3.csv(
-    "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv",
-    function(d) {
-      data.set(d.iso_code)
+    COVID_DATASET,
+    function (d) {
+      data.set(d.iso_code);
+
+      if (!(d.date in covid_data)) {
+        covid_data[d.date] = {};
+      }
+      covid_data[d.date][d.iso_code] = {
+        total_cases: d.total_cases,
+        new_cases: d.new_cases,
+        total_deaths: d.total_deaths,
+        new_deaths: d.new_deaths
+      }
     }
   ),
 ]).then(function (loadData) {
   let topo = loadData[0];
 
   let mouseOver = function (e, d) {
-    d3.selectAll('.country')
-      .style('opacity', 0.5)
+    d3.selectAll(".country").style("opacity", 0.5);
 
-    d3.select(this)
-      .style('opacity', 1)
-      .style('stroke', 'black')
+    d3.select(this).style("opacity", 1).style("stroke", "black");
 
-    tooltip.style("left", e.pageX + "px")
+    tooltip
+      .style("left", e.pageX + "px")
       .style("top", e.pageY + "px")
-      .style('opacity', 1)
-      .text(d.properties.name)
+      .style("opacity", 1)
+      .text(d.properties.name);
   };
 
-  let mouseLeave = function() {
-    d3.selectAll('.country')
-      .style('opacity', 1)
-      .style('stroke', 'transparent')
-    
-    tooltip.style('opacity', 0)
-  }
+  let mouseLeave = function () {
+    d3.selectAll(".country").style("opacity", 1).style("stroke", "transparent");
+
+    tooltip.style("opacity", 0);
+  };
 
   // Draw choropleth
-  svg.append("g")
+  svg
+    .append("g")
     .selectAll("path")
     .data(topo.features)
     .join("path")
@@ -68,21 +92,26 @@ Promise.all([
     // Draw countries
     .attr("d", d3.geoPath().projection(projection))
 
-    .attr('data-name', function(d) {
-      return d.properties.name
+    .attr("data-name", function (d) {
+      return d.properties.name;
     })
 
     // Set color of countries
     .attr("fill", function (d) {
-      return "gray";
+      covid_data_country = covid_data['2021-08-02'][d.id]
+      if (covid_data_country) {
+        return rColorScale(covid_data_country.total_cases);
+      } else {
+        return "grey";
+      }
     })
 
-    .attr('class', function(d) {
-      return 'country'
+    .attr("class", function (d) {
+      return "country";
     })
-    .attr('id', function(d) {
-      return d.id
+    .attr("id", function (d) {
+      return d.id;
     })
-    .on('mouseover', mouseOver)
-    .on('mouseleave', mouseLeave)
+    .on("mouseover", mouseOver)
+    .on("mouseleave", mouseLeave);
 });
